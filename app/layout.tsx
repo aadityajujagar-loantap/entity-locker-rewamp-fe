f1db1daa0b4b33e848b8e82e28f3d6ca1c14a854
Aaditya Jujagar <aditya.jujagar@loantap.in>
2026-07-22 10:59:50 +0530

    font family fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
-
-const nunitoSans = Nunito_Sans({
-  subsets: ["latin"],
-  style: ["normal", "italic"],
-  axes: ["opsz"],
-  display: "swap",
-  variable: "--font-nunito-sans",
-});
 
 export const metadata: Metadata = {
   title: "Entity Locker",
-  description: "Entity Locker rewamped app - by Aaditya Jujagar",
+  description: "Entity Locker revamped app",
 };
 
 export default function RootLayout({
@@ -21,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={nunitoSans.variable} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
